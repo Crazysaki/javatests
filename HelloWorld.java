9b6cc2e3b878e414e65784f6cfd41854c537dd33
Liz Carver <elc@dell-gx620.(none)>
2015-09-07 14:40:23 +0100

    print from the prgram

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    static String yolo = "Daad";
+	static String swag = "SUUUCKS";
+    public static void main(String[] args) {
+        System.out.println(yolo+" "+swag);
+    }
+
+}
+
+
+
