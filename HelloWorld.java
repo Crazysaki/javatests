dcc037367259707d92cbf08ec4d01fcbc351cb30
Liz Carver <elc@dell-gx620.(none)>
2015-09-07 15:25:19 +0100

    data.txt

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,12 +1,25 @@
+import java.io.*;
+
 public class HelloWorld {
+    public static void main(String [] args) {
+        String fileName = "data.txt";
+        String line = null;
 
-    static String yolo = "Daad";
-	static String swag = "SUUUCKS";
-    public static void main(String[] args) {
-        System.out.println(yolo+" "+swag);
+        try {
+            FileReader fileReader =  new FileReader(fileName);
+            BufferedReader bufferedReader =  new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }    
+
+            bufferedReader.close();            
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println( "Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println( "Error reading file '" + fileName + "'");                   
+        }
     }
-
 }
-
-
-
