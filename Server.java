955757868d7f5ce515a8cdaecc7ed7f5083dff00
Liz Carver <elc@dell-gx620.(none)>
2015-09-08 10:21:26 +0100

    bytes

--- Server.java
+++ Server.java
@@ -19,11 +19,12 @@ public class Server {
         @Override
         public void handle(HttpExchange t) throws IOException {
             String response = "This is the response";
-            t.sendResponseHeaders(200, response.length());
+	    byte bytes[] = response.getBytes();
+            t.sendResponseHeaders(200, bytes.length);
             OutputStream os = t.getResponseBody();
-            os.write(response.getBytes());
+            os.write(bytes);
             os.close();
         }
     }
 
-}
+} 
