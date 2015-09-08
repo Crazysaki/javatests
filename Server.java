76c77088a2487aa26f73fa66ea78349a8d47841a
Liz Carver <elc@dell-gx620.(none)>
2015-09-08 10:48:01 +0100

    data.txt innit

--- Server.java
+++ Server.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -11,6 +12,7 @@ public class Server {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/test", new MyHandler());
+	server.createContext("/dad", new DadHandler());
         server.setExecutor(null); // creates a default executor
         server.start();
     }
@@ -26,5 +28,34 @@ public class Server {
             os.close();
         }
     }
+    static class DadHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange t) throws IOException {
+        String fileName = "data.txt";
+        String line = null;
+        String contents = "";
 
+        try {
+            FileReader fileReader =  new FileReader(fileName);
+            BufferedReader bufferedReader =  new BufferedReader(fileReader);
+
+            while((line = bufferedReader.readLine()) != null) {
+                contents = contents + line + '\n';
+            }    
+
+            bufferedReader.close();            
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println( "Unable to open file '" + fileName + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println( "Error reading file '" + fileName + "'");                   
+        }
+	    byte bytes[] = contents.getBytes();
+            t.sendResponseHeaders(200, bytes.length);
+            OutputStream os = t.getResponseBody();
+            os.write(bytes);
+            os.close();
+        }
+    }
 } 
